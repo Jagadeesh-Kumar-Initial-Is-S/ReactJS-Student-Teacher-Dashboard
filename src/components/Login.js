fa6550a8891835bca8ef186925d232a99703fcc7
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2022-12-15 18:52:03 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
     return <AuthContext.Consumer>
             {(ctx) => {
                 return <div>
-                    <h1>The user name and password is empty and so you may click on button named as Login to login at this moment.<br/></h1>
+                    <h1>The user name and password is empty and so you may click on button named as "Login" to login at this moment.<br/></h1>
                     <h4>Login</h4>
                     <form onSubmit={()=>{
                         ctx.setLoggedIn(true);
